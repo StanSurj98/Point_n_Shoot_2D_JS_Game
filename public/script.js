417b324ec179dd8bd431f8e3ca9ca705d62d9d1e
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-25 20:12:58 -0700

    setup timestamp for raven spawns, update comments

--- public/script.js
+++ public/script.js
@@ -4,16 +4,22 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Will hold all objects of ravens later
+// Ravens to spawn at constant time for good && bad computers
+let timeToNextRaven = 0; // accumulates time between frames
+let ravenInterval = 500; // when accumulated "time" match interval - spawn raven & reset
+let lastTime = 0; // hold value of 'timestamp' from initial loop, constantly changes
+
+
+// Hold all raven instances
 let ravens = [];
 
 class Raven {
   constructor(){
     this.width = 100;
     this.height = 50;
-    // just to the right of view so they fly in
+    // just to right of canvas so they fly in
     this.x = canvas.width; 
-    // so that each raven spawns within canvas and not above/below it | btw the () are necessary
+    // ensure spawns within canvas and not above/below it | btw, () are necessary
     this.y = Math.random() * (canvas.height - this.height); 
     this.directionY = Math.random() * 5 + 3;
     this.directionX = Math.random() * 5 - 2.5;
@@ -27,14 +33,26 @@ class Raven {
 }
 
 
-const raven = new Raven();
-
+// "Timestamp" is default JS behavior with reqAnimFrame() func
 const animate = (timestamp) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  raven.update();
-  raven.draw();
 
-  // Recursion animation calls
+  // Essentially "how fast your computer can serve the next frame"
+  let deltaTime = timestamp - lastTime; // on my comp: 16.65ms between frames - fast
+  lastTime = timestamp;
+  
+  // Accumulate the "time in between" new frames
+  timeToNextRaven += deltaTime;
+  
+  // MEANING: whether computer is fast or slow - hard cap to spawn a raven at Interval
+    // slow computers have big deltaTime (longer time between new frames)
+    // REGARDLESS - game waits "ravenInterval" ms
+  if (timeToNextRaven > ravenInterval) {
+    ravens.push(new Raven());
+    timeToNextRaven = 0;
+  }
+
+
   requestAnimationFrame(animate);
 }
-animate();
+// animate();
