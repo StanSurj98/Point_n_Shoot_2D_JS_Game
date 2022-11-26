2875224abd830c3e95c8df749193d3a23b301f90
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-25 19:45:28 -0700

    initial raven class and 1 tester flies on screen 👀

--- public/script.js
+++ public/script.js
@@ -3,3 +3,38 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// Will hold all objects of ravens later
+let ravens = [];
+
+class Raven {
+  constructor(){
+    this.width = 100;
+    this.height = 50;
+    // just to the right of view so they fly in
+    this.x = canvas.width; 
+    // so that each raven spawns within canvas and not above/below it | btw the () are necessary
+    this.y = Math.random() * (canvas.height - this.height); 
+    this.directionY = Math.random() * 5 + 3;
+    this.directionX = Math.random() * 5 - 2.5;
+  }
+  update() {
+    this.x -= this.directionX;
+  }
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+
+const raven = new Raven();
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  raven.update();
+  raven.draw();
+
+  // Recursion animation calls
+  requestAnimationFrame(animate);
+}
+animate();
